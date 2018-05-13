c7e420673716961e73e0426896e7681d37caa825
fangqk1991 <me@fangqk.com>
2018-05-13 20:04:35 +0800

    Example project

---
+++ Example/FC-Utils/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  FC-Utils_Example
+//
+//  Created by fang on 2018/5/13.
+//  Copyright © 2018 fangqk1991. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
---
+++ Example/FC-Utils/MasterViewController.m
@@ -0,0 +1,82 @@
+//
+//  MasterViewController.m
+//  FC-Utils_Example
+//
+//  Created by fang on 2018/5/13.
+//  Copyright © 2018 fangqk1991. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+@interface MasterViewController ()
+
+@property (strong, nonatomic) NSArray *infos;
+
+@end
+
+@implementation MasterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    __weak __typeof(self)weakSelf = self;
+    
+    _infos = @[
+               @[
+                   @{
+                       @"text": @"支付宝",
+                       @"event":
+                           ^{
+//                               [AlipayViewController fc_pushToNavigation:weakSelf.navigationController];
+                           }
+                       }
+                   ]
+               ];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table View
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return _infos.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSArray *array = _infos[section];
+    return array.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    NSArray *array = _infos[indexPath.section];
+    NSDictionary *info = array[indexPath.row];
+    cell.textLabel.text = info[@"text"];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    
+    NSArray *array = _infos[indexPath.section];
+    NSDictionary *info = array[indexPath.row];
+    
+    void (^handler)(void) = info[@"event"];
+    if(handler)
+    {
+        handler();
+    }
+}
+
+@end
