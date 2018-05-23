774d8f67e24cc58240c94b44972ebbe556cb5c69
fangqk1991 <me@fangqk.com>
2018-05-23 13:20:28 +0800

    task examples

--- Example/FC-Utils/FCTask/TaskListVC.m
+++ Example/FC-Utils/FCTask/TaskListVC.m
@@ -9,6 +9,7 @@
 #import "TaskListVC.h"
 #import "FCAsyncTask.h"
 #import "NSLogger.h"
+#import "MBProgressHUD.h"
 
 @interface TaskListVC ()
 
@@ -37,10 +38,30 @@
 
 - (void)normalTask
 {
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.mode = MBProgressHUDModeAnnularDeterminate;
+    hud.label.text = @"Loading";
+    
     FCAsyncTask *task = [[FCAsyncTask alloc] init];
+    task.onPreExecuteBlock = ^{
+        hud.progress = 0;
+    };
+    task.onProgressUpdateBlock = ^(NSUInteger current, NSUInteger total) {
+        hud.progress = 1.0 * current / total;
+        hud.label.text = [NSString stringWithFormat:@"%@ / %@", @(current), @(total)];
+        LoggerApp(3, @"Progress: %@ / %@", @(current), @(total));
+    };
     task.doInBackgroundBlock = ^id(FCAsyncTask *_self) {
         LoggerApp(3, @"doInBackgroundBlock");
+        for(int i = 0; i < 10; ++i)
+        {
+            [_self updateProgress:i total:10];
+            sleep(1);
+        }
         return nil;
+    };
+    task.onPostExecuteBlock = ^(id result) {
+        [hud hideAnimated:YES];
     };
     [task execute];
 }
