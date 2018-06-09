fa590f5ec27189c024468846525fdc91878835dd
fangqk1991 <me@fangqk.com>
2018-06-09 21:41:44 +0800

    UIImage+Color

---
+++ FC-Utils/Core/UIImage+Color.h
@@ -0,0 +1,14 @@
+//
+//  UIImage+Color.h
+//  FC-Utils
+//
+//  Created by fang on 2018/6/9.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage(Color)
+
++ (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size;
+
+@end
